d66c9c34c2582af3aeb5df590754d8ac08bd67ea
SongChengCheah <112138670+Crepopcorn@users.noreply.github.com>
2024-10-08 10:21:57 +0800

    Update database.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -2,9 +2,7 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# MongoDB connection
 client = MongoClient(os.getenv('MONGO_URI'))
 db = client['environment_data']
