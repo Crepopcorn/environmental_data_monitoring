2fb6968a2a81a5270b3fb1cc9d6f9a9c9863b729
SongChengCheah <112138670+Crepopcorn@users.noreply.github.com>
2024-10-08 10:16:36 +0800

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,6 @@
-// frontend/src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_BACKEND_URL); // Ensure this matches the backend URL (e.g., http://localhost:4000)
+const socket = io(process.env.REACT_APP_BACKEND_URL); // Ensure .env location matches backend URL (e.g., http://localhost:4000)
 
 export default socket;
 
